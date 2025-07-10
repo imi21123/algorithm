06897595477c427108e6b13f99892e4d6d58f9d7
Chaeyeon Han <91395969+imi21123@users.noreply.github.com>
2025-07-10 14:34:59 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int l, p, a, b, c, d, e;
+    scanf("%d %d", &l, &p);
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+    printf("%d %d %d %d %d", a - l * p, b - l * p, c - l * p, d - l * p, e - l * p);
+}
