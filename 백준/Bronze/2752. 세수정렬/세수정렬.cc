96450c20a0b6b70bb2dfe12e1ccc24811749d3de
Chaeyeon Han <91395969+imi21123@users.noreply.github.com>
2025-07-10 14:31:43 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[5];
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	for (int i = 0; i < 3; i++)
+		cin >> arr[i];
+
+	sort(arr, arr + 3);
+
+	for (int i = 0; i < 3; i++)
+		cout << arr[i] << " ";
+}
