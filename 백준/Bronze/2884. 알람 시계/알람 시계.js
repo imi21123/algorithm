4359c35b22211ac8a32e0a44f2f24a734f759f64
Chaeyeon Han <91395969+imi21123@users.noreply.github.com>
2025-07-10 14:29:25 +0900

    [Bronze III] Title: 알람 시계, Time: 96 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.js
+++ 백준/Bronze/2884. 알람 시계/알람 시계.js
@@ -1,17 +1,21 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let h = Number(input[0].split(' ')[0]);
-let m = Number(input[1].split(' ')[0]);
+let h = Number(input[0].split(' ')[0])
+let m = Number(input[0].split(' ')[1])
 
 if (m >= 45) {
-    console.log(h, m - 45);
+    m -= 45;
 }
 else {
     if (h > 0) {
-        console.log(h - 1, m + 15);
+        h -= 1;
+        m += 15;
     }
     else {
-        console.log(23, m + 15);
+        h = 23;
+        m += 15;
     }
 }
+
+console.log(h, m);
