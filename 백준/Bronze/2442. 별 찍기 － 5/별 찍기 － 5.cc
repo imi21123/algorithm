51846e8f0bbed9261a2d60a45309d527b779df5f
Chaeyeon Han <91395969+imi21123@users.noreply.github.com>
2025-07-10 14:32:42 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n - i; j++)
+			cout << ' ';
+		for (int j = 1; j <= i * 2 - 1; j++)
+			cout << '*';
+		cout << "\n";
+	}
+}
