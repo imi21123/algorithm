1eb0a382d2efd800d2f4c91223f6d26b5a30cdca
Chaeyeon Han <91395969+imi21123@users.noreply.github.com>
2025-07-13 22:59:06 +0900

    [Silver III] Title: N과 M (5), Time: 44 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m, check[10], ans[10], num[10];
+void go(int location) {
+	if (location == m) {
+		for (int i = 0; i < m; i++)
+			printf("%d ", ans[i]);
+		printf("\n");
+		return;
+	}
+	for (int i = 0; i < n; i++) {
+		if (!check[i]) {
+			ans[location] = num[i];
+			check[i] = 1;
+			go(location + 1);
+			check[i] = 0;
+		}
+	}
+	return;
+}
+int main() {
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++)
+		scanf("%d", &num[i]);
+	sort(num, num + n);
+	go(0);
+}
